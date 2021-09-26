a32611aa793eef77c43a141428dc88f09f99294b
Leiry <80017244+little-leiry@users.noreply.github.com>
2021-09-26 22:36:27 +0800

    Update normal-2.py

--- normal-2.py
+++ normal-2.py
@@ -9,7 +9,7 @@ import time
 import random
 
 base_path="E:/CuPerFuzzer/normal-2/"
-apk_base_path2 = "E:/CuPerFuzzer/apk-2/"
+apk_base_path2 = "E:/CuPerFuzzer/apk-2/" # test apps for the dual-app mode
 
 ########################################
 # select a seed randomly
